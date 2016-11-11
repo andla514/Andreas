9cce9e56d045c5c874c8fb6881ea2eb5a054244f
unknown <CASJO385@CYD42.ad.liu.se>
2016-11-11 15:02:58 +0100

    update ion  brach character

--- Character.h
+++ Character.h
@@ -3,11 +3,26 @@
 #define CHARACTER_H
 #include "Game.h"
 #include "Bomb.h"
+#include <string>
 
 class Character
 {
     public:
     Character();
+	Character(shared_prt<Game> our_game, int player_number);
+	get_pos();
+	
+	
+	void move_player();
+	
+	private:
+	
+	std::string up{};
+	std::string down{};
+	std::string left{};
+	std::string right{};
+	int xpos{};
+	int ypos{};
 };
 
 #endif
