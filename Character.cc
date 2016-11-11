7f590d250fe2194305043906f74d83039b062ad1
unknown <CASJO385@CYD42.ad.liu.se>
2016-11-11 14:05:55 +0100

    Created a constructor for character

--- Character.cc
+++ Character.cc
@@ -1,7 +1,48 @@
 #include "Game.h"
 #include "Character.h"
 #include "Bomb.h"
+#include <stdio.h>
 
 //-----------------CONSTRUCTOR--------------
 Character::Character()
 {}
+
+Character(shared_prt<Game> our_game, int player_number)
+{
+	switch(player_number)
+	{
+		case 1:
+			up = 'W';
+			down = 'S';
+			left = 'A'
+			right = 'D';
+			xpos = 1;
+			ypos = 1;
+			break;
+		case 2:
+			up = '8';
+			down = '5';
+			left = '4';
+			right = '6';
+			xpos = our_game.get_columns() - 1;
+			ypos = our_game.get_rows() - 1;
+			break;
+		case 3:
+			up = '8';
+			down = '5';
+			left = '4';
+			right = '6';
+			xpos = our_game.get_columns() - 1;
+			ypos = our_game.get_rows() - 1;
+	
+	
+}
+
+void Character::move_player()
+{
+	
+	
+	
+	
+	
+}
