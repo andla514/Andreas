e03faf8b9ad12dfd32dfb85ff5664055a2f2f3d7
unknown <CASJO385@CYD42.ad.liu.se>
2016-11-11 15:02:58 +0100

    upadte on branch character

--- Character.cc
+++ Character.cc
@@ -2,6 +2,7 @@
 #include "Character.h"
 #include "Bomb.h"
 #include <stdio.h>
+#include "SFML/Graphics.hpp"
 
 //-----------------CONSTRUCTOR--------------
 Character::Character()
@@ -12,10 +13,10 @@ Character(shared_prt<Game> our_game, int player_number)
 	switch(player_number)
 	{
 		case 1:
-			up = 'W';
-			down = 'S';
-			left = 'A'
-			right = 'D';
+			up = "sf::Keyboard::W";
+			down = "sf::Keyboard::S";
+			left = "sf::Keyboard::A";
+			right = "sf::Keyboard::D";
 			xpos = 1;
 			ypos = 1;
 			break;
@@ -28,10 +29,10 @@ Character(shared_prt<Game> our_game, int player_number)
 			ypos = our_game.get_rows() - 1;
 			break;
 		case 3:
-			up = '8';
-			down = '5';
-			left = '4';
-			right = '6';
+			up = "sf::Keyboard::Numpad8";
+			down = "sf::Keyboard::Numpad5";
+			left = "sf::Keyboard::Numpad4";
+			right = "sf::Keyboard::Numpad6";
 			xpos = our_game.get_columns() - 1;
 			ypos = our_game.get_rows() - 1;
 	
