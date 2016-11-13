4385b221c8c90aa18cfc4afe7979ebecc6fb1a97
casjo385 <casperpwjohansson@gmail.com>
2016-11-13 11:12:35 +0100

    Update on Character, included the sfml libary and fix the makefile to include sfml. Character is currently not working

--- Character.h
+++ Character.h
@@ -4,23 +4,31 @@
 #include "Game.h"
 #include "Bomb.h"
 #include <string>
+#include <memory>
+//#include "SFML/Graphics.hpp" //bara sålänge kontrollerna initializeras här
 
 class Character
 {
     public:
     Character();
-	Character(shared_prt<Game> our_game, int player_number);
-	get_pos();
-	
-	
+	Character(std::shared_ptr<Game>);
+	//Character(shared_prt<Game> our_game, int player_number);
+	int get_xpos();
+	int get_ypos();
+	//void set_ypos(int y);
+	//void set_pos(int x);
+
 	void move_player();
 	
 	private:
-	
-	std::string up{};
-	std::string down{};
-	std::string left{};
-	std::string right{};
+	std::shared_ptr<Game> game_ptr{};
+	std::string up{};//sf::Keyboard::W};
+	std::string down{};//sf::Keyboard::S};
+	std::string left{};//sf::Keyboard::A};
+	std::string right{};//sf::Keyboard::D};
+	std::string place_bomb{};
+	int life{};
+	int bombs{};
 	int xpos{};
 	int ypos{};
 };
