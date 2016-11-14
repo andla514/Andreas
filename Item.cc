1bfe3e39f3ad6cf06c14a9795654fea5dc845a73
Morten Eriksson <mollerup94@gmail.com>
2016-11-14 13:09:30 +0100

    första implementation

--- Item.cc
+++ Item.cc
@@ -1,7 +1,52 @@
-#include "Game.h"
-#include "Character.h"
+
 #include "Item.h"
 
-//-----------------CONSTRUCTOR--------------
-Item::Item()
+// -------------------------------------------------------------------------------------
+
+Item::Item(int const & row, int const & col)
+    :item_row{row}, item_col{col}
 {}
+
+
+void Item::change_character()
+//Character const & new_character)
+{
+//	character_ptr{} = new_character;	
+}
+
+
+bool Item::has_character()
+{
+//	return (character_ptr != nullptr);
+    return true;
+}
+
+// --------------------------------------------------------------------------------------
+// Item_Inc_Bombs
+// --------------------------------------------------------------------------------------
+void Item_Inc_Bombs::give_power_up(Character & character)
+{
+//	character.add_bomb(1);
+}
+
+void Item_Inc_Bombs::draw_graphics()
+{
+}
+
+
+// --------------------------------------------------------------------------------------
+// Item_Inc_Exp_Rad
+// --------------------------------------------------------------------------------------
+void Item_Inc_Exp_Rad::give_power_up(Character & character)
+{
+//	character.increase_exp_rad(2);
+}
+
+void Item_Inc_Exp_Rad::draw_graphics()
+{
+}
+// --------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------
+
--- Item.h
+++ Item.h
@@ -1,13 +1,70 @@
 
 #ifndef ITEM_H
 #define ITEM_H
-#include "Game.h"
-#include "Character.h"
+
+class Item;
+class Item_Inc_Bombs;
+class Item_Inc_Exp_Rad;
+
 
 class Item
 {
-    public:
-    Item();
+public:
+    Item(int const & row, int const & col);
+    virtual ~Item() = default;
+    void change_character();         //Character &current_character);
+    bool has_character();
+    virtual void give_power_up() = 0;
+    
+    virtual void draw_graphics() = 0;
+    
+    
+    int item_row{};
+    int item_col{};
+    
+};
+
+
+
+class Item_Inc_Bombs:public Item
+{
+public:
+    using Item::Item;
+    ~Item_Inc_Bombs() = default;
+    void give_power_up(Character & character) override;
+    
+    
+    void draw_graphics() override;
+};
+
+
+class Item_Inc_Exp_Rad:public Item
+{
+public:
+    using Item::Item;
+    ~Item_Inc_Exp_Rad() = default;
+    void give_power_up(Character & character) override;	
+    
+    void draw_graphics() override;
+};
+
+
+class Item_3:public Item
+{
+public:
+    using Item::Item;
+    ~Item_3() = default;
+    
+    void draw_graphics() override;
+};
+
+class Item_4:public Item
+{
+public:
+    using Item::Item;
+    ~Item_4() = default;
+    
+    void draw_graphics() override;
 };
 
 #endif
