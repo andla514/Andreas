f43e3d4deee8bde5b792bfa2d2a52768b34e955e
unknown <andla514@CYD48.ad.liu.se>
2016-11-17 14:12:08 +0100

    added removed files

---
+++ Character.cc
@@ -0,0 +1,163 @@
+//#include <stdio.h>
+#include <memory>
+#include "SFML/Graphics.hpp"
+#include "Game.h"
+#include "Character.h"
+#include "Bomb.h"
+#include "Item.h"
+//#include <iostream> //Används ej?
+//#include <utility>  //bara för move i testkonstruktorn
+//-----------------CONSTRUCTOR--------------
+Character::Character()
+{
+}
+Character::Character(std::shared_ptr<Game> our_game, int player_number)
+    : game_ptr{our_game}, player_number{player_number}
+{
+    switch (player_number)
+    {
+    case 1:
+	col = 1;
+	row = 1;
+	up = sf::Keyboard::W;
+	down = sf::Keyboard::S;
+	left = sf::Keyboard::A;
+	right = sf::Keyboard::D;
+	bomb = sf::Keyboard::Tab;
+	break;
+    case 2:
+	col = our_game->get_columns() - 2;
+	row = our_game->get_rows() - 2;
+	up = sf::Keyboard::Up;
+	down = sf::Keyboard::Down;
+	left = sf::Keyboard::Left;
+	right = sf::Keyboard::Right;
+	bomb = sf::Keyboard::Tab;
+	break;
+    case 3:
+	col = our_game->get_columns() - 2;
+	row = 1;
+	up = sf::Keyboard::Numpad8;
+	down = sf::Keyboard::Numpad5;
+	left = sf::Keyboard::Numpad4;
+	right = sf::Keyboard::Numpad6;
+	bomb = sf::Keyboard::Tab;
+	break;
+    case 4:
+	col = 1;
+	row = our_game->get_rows() - 2;
+	up = sf::Keyboard::T;
+	down = sf::Keyboard::G;
+	left = sf::Keyboard::F;
+	bomb = sf::Keyboard::Tab;
+	break;
+    }
+}
+//-----------------FUNCTION--------------
+int Character::get_col() const
+{
+    return col;
+}
+int Character::get_row() const
+{
+    return row;
+}
+void Character::add_bomb(int number)
+{
+    bombs += number;
+}
+void Character::increase_exp_rad(int number)
+{
+    //Bomb_settings.radius += number;
+}
+void Character::increase_exp_time(int number)
+{
+    //bomb_settings.explosion_delay += number;
+}
+void Character::update()
+{
+    if (life > 0)
+    {
+		if (is_moving)
+		{
+			smooth_move();
+		}
+		else
+		{
+			move_player();
+		}
+			
+		if (game_ptr->is_standing_in_fire(row, col) && !(is_immortal))
+		{
+	    	hurt_player();
+	    	is_immortal = true;
+		}
+		else if (!(game_ptr->is_standing_in_fire(row, col)))
+		{
+	   		is_immortal = false;
+		}
+		make_bomb();
+		if (game_ptr->is_standing_on_item(row, col))
+		{
+	    	use_item(game_ptr->get_item_at(row, col));
+		}
+    }
+}
+void Character::make_bomb()
+{
+    if (sf::Keyboard::isKeyPressed(bomb) && bombs > 0 && !(is_immortal))
+    {
+	//anropa bomb-construktor
+    }
+}
+void Character::use_item(Item pickup)
+{
+    //pickup.give_power_up(*this)
+}
+void Character::smooth_move()
+{
+    is_moving = false;
+}
+void Character::hurt_player()
+{
+    life -= 1;
+}
+void Character::move_player()
+{
+    int col_inc{0};
+	int row_inc{0};
+    if (sf::Keyboard::isKeyPressed(up))
+    {
+		row_inc -= 1;
+    }
+    if (sf::Keyboard::isKeyPressed(down))
+    {
+		row_inc += 1;
+    }
+    if (sf::Keyboard::isKeyPressed(left))
+    {
+		col_inc -= 1;
+    }
+    if (sf::Keyboard::isKeyPressed(right))
+    {
+		col_inc += 1;
+    }
+    if (col_inc == 0 && row_inc == 0)
+    {
+		is_moving = false;
+    }
+    else if((col_inc == 0 || row_inc == 0) && game_ptr->can_move_to(row + row_inc, col + col_inc))
+    {
+		row += row_inc;
+		col += col_inc;
+		is_moving = true;
+    } else if(game_ptr->can_move_to(row + row_inc, col))
+	{
+		row += row_inc;
+		is_moving = true;
+	} else if(game_ptr->can_move_to(row, col + col_inc))
+	{
+		col += col_inc;
+		is_moving = true;
+	}
+}
---
+++ Game.h
@@ -0,0 +1,55 @@
+
+#ifndef GAME_H
+#define GAME_H
+class Game;
+#include <map>
+#include "Bomb.h"
+#include "Item.h"
+#include "Character.h"
+#include <string>
+#include "Explosion.h"
+#include "Matrix_Map.h"
+#include <memory>
+/*
+    0   :   Ground
+    1   :   Box
+    2   :   Item
+    3   :   Bomb
+    4   :   Explosion
+    5   :   Wall
+*/
+class Game
+{
+    public:
+    Game();
+    void update() noexcept;
+    void draw_graphics() const noexcept;
+    int get_element(int row, int col) const noexcept;
+    void set_element(int row, int col, int new_value) noexcept;
+    int get_rows() const noexcept;
+    int get_columns() const noexcept;
+    // Bools
+    bool is_standing_in_fire(int row, int col) const noexcept;
+    bool is_standing_on_item(int row, int col) const noexcept;
+    bool can_move_to(int row, int col) const noexcept;
+    bool is_wall(int row, int col) const noexcept;
+    bool is_box(int row, int col) const noexcept;
+    // Handle reference lists
+    void add_item(int row, int col, Item && new_item) noexcept;
+    void add_bomb(int row, int col, Bomb && new_bomb) noexcept;
+    void add_explosion(int row, int col, Explosion && new_explosion) noexcept;
+    Item get_item_at(int row, int col);
+    void remove_bomb(int row, int col) noexcept;
+    void remove_explosion(int row, int col) noexcept;
+    void add_characters(int number_of_players, std::shared_ptr<Game> our_game);
+    Character & get_character_reference(int player_number);
+    Bomb & get_bomb_reference(int row, int col);
+    // Data
+    private:
+    Matrix_Map our_matrix{};
+    std::map<std::string, Item> item_list{};
+    std::map<std::string, Bomb> bomb_list{};
+    std::map<std::string, Explosion> explosion_list{};
+    std::map<int, Character> character_list{};
+};
+#endif
