d24423e8c2042e9a8779755a7b64691ec9eba2d5
Tommy Karlsson <tomka260@student.liu.se>
2016-11-17 14:48:51 +0100

    Mainfix

--- Main.cc
+++ Main.cc
@@ -4,8 +4,6 @@
 #include "Main.h"
 #include <stdlib.h>
 #include <iostream>
-#include <thread>
-#include <chrono>
 #include <memory>
 #include "SFML/Graphics.hpp"
 
@@ -23,30 +21,36 @@ void Main::start_non_graphic()
     game_timer.restart();
     while(is_running)
     {
-        system("CLS");
-        our_game->update();
-
-        for(int i = 0; i < 3; i++)
+        sf::Event our_event;
+        while (our_window.pollEvent(our_event))
         {
-            Character our_character = our_game->get_character_reference(i + 1);
-            old_value[i] = our_game->get_element(our_character.get_row(), our_character.get_col());
-            our_game->set_element(our_character.get_row(), our_character.get_col(), 6);
+            if (our_event.type == sf::Event::Closed)
+            {
+                our_window.close();
+            }
         }
-
-        our_game->draw_graphics();
-
-    for(int i = 0; i < 3; i++)
+        
+        if(game_timer.is_done())
         {
-            Character our_character = our_game->get_character_reference(i + 1);
-            our_game->set_element(our_character.get_row(), our_character.get_col(), old_value[i]);
+            system("CLS");
+            our_game->update();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Character our_character = our_game->get_character_reference(i + 1);
+                old_value[i] = our_game->get_element(our_character.get_row(), our_character.get_col());
+                our_game->set_element(our_character.get_row(), our_character.get_col(), 6);
+            }
+
+            our_game->draw_graphics();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Character our_character = our_game->get_character_reference(i + 1);
+                our_game->set_element(our_character.get_row(), our_character.get_col(), old_value[i]);
+            }
+            game_timer.restart();
         }
-
-        game_timer.stop();
-        std::cout << "Runtime: " << game_timer.elapsed_time() << std::endl;
-        std::cout << "Time left: " << game_timer.time_left() << "ms" << std::endl;
-        //std::cout << "Character position: " << "Row: " << our_character.get_row() << " Col: " << our_character.get_col() << std::endl;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(game_timer.time_left()));
-        game_timer.restart();
+        sf::sleep(sf::milliseconds(2));
     }
 }
