5a71172402d2f4e3913b7ebcc41f28a26a1a4bd8
unknown <casjo385@CYD49.ad.liu.se>
2016-11-18 10:42:54 +0100

    nagot

---
+++ Main.h
@@ -0,0 +1,28 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+#include "Game.h"
+#include "Character.h"
+#include "Timer.h"
+#include <stdlib.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <memory>
+#include "SFML/Graphics.hpp"
+
+class Main
+{
+    public:
+    Main();
+	sf::RenderWindow Window;
+
+    void start_non_graphic();
+
+    private:
+    std::shared_ptr<Game> our_game;
+    bool is_running{true};
+    Timer game_timer{1};
+};
+
+#endif
---
+++ Main_game.cpp
@@ -0,0 +1,9 @@
+//#include "Main.h"
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+
+    Main our_main;
+    our_main.start_non_graphic();
+}
