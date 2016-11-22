ebd909dfbacd4959395eebe10e049104067d6219
Tommy Karlsson <tomka260@student.liu.se>
2016-11-22 12:58:34 +0100

    Item integrerat

--- Item.cc
+++ Item.cc
@@ -1,9 +1,9 @@
 
 #include "Item.h"
+
 #include "Game.h"
 #include "Character.h"
 #include "Bomb.h"
-#include "SFML/Graphics.hpp"
 
 
 // --------------------------------------------------------------------------------------
@@ -11,7 +11,14 @@
 // --------------------------------------------------------------------------------------
 Item::Item(int const & row, int const & col)
     :item_row{row}, item_col{col}
-{}
+{
+	if(!powerup_Texture.loadFromFile("Textures/Powerups/BombPowerup.png"))
+	{
+		throw std::logic_error("Can't load Item texture");
+	}
+	
+	powerup_Sprite.setTexture(powerup_Texture);	
+}
 
 
 // --------------------------------------------------------------------------------------
@@ -23,8 +30,20 @@ void Item_Inc_Bombs::give_power_up(Character & character)
 	character.add_bomb(1);
 }
 
-void Item_Inc_Bombs::draw_graphics(sf::RenderTexture &)
-{}
+void Item_Inc_Bombs::draw_graphics(sf::RenderTexture & our_window)
+{
+	//sf::Sprite to_draw{powerup_Texture};
+	powerup_Sprite.setPosition(64 * item_col + 16, 64 * item_row + 16);
+	our_window.draw(powerup_Sprite);
+	/*
+	sf::Texture Increase_Number_of_Maximum_Bombs_Texture; 
+	sf::Sprite Increase_Number_of_Maximum_Bombs_Sprite;
+	if(!Increase_Number_of_Maximum_Bombs.loadFromFile("Textures/Powerups/BombPowerup.png"))
+		{
+		throw std::logic_error("Can't load Item 'Increase Explosion Radius' texture");
+		}
+	*/
+}
 
 
 // --------------------------------------------------------------------------------------
@@ -36,8 +55,17 @@ void Item_Inc_Exp_Rad::give_power_up(Character & character)
 	character.increase_exp_rad(2);
 }
 
-void Item_Inc_Exp_Rad::draw_graphics(sf::RenderTexture &)
-{}
+void Item_Inc_Exp_Rad::draw_graphics(sf::RenderTexture & our_window)
+{
+	/*
+	sf::Sprite Increase_Explosion_Radius;
+	if(!Increase_Explosion_Radius.loadFromFile("Textures/Powerups/FlamePowerup" + ".png"))
+		{
+		throw std::logic_error("Can't load Item 'Increase Explosion Radius' texture");
+		}
+	*/
+}
+
 
 // --------------------------------------------------------------------------------------
 // Item_Inc_Life
@@ -48,9 +76,16 @@ void Item_Inc_Life::give_power_up(Character & character)
 	//character.add_life(1);
 }
 
-
 void Item_Inc_Life::draw_graphics(sf::RenderTexture &)
-{}
+{
+	/*
+	sf::Sprite Increase_Life;
+	if(!Increase_Life.loadFromFile("Textures/Powerups/LifePowerup" + ".png"))
+		{
+		throw std::logic_error("Can't load Item 'Increase Life' texture");
+		}
+	*/
+}
 
 // --------------------------------------------------------------------------------------
 // Item_Inc_Exp_Time
@@ -62,4 +97,12 @@ void Item_Inc_Exp_Time::give_power_up(Character & character)
 }
 
 void Item_Inc_Exp_Time::draw_graphics(sf::RenderTexture &)
-{}
+{
+	/*
+	sf::Sprite Increase_Explosion_Duration_Time;
+	if(!Increase_Explosion_Duration_Time.loadFromFile("Textures/Powerups/BombTimePowerup" + ".png"))
+		{
+		throw std::logic_error("Can't load Item 'Increase Explosion Duration Time' texture");
+		}
+	*/
+}
--- Item.h
+++ Item.h
@@ -1,15 +1,18 @@
 
 #ifndef ITEM_H
 #define ITEM_H
+#include "Tile.h"
+#include "SFML/Graphics.hpp"
 class Item;
 class Item_Inc_Bombs;
 class Item_Inc_Exp_Rad;
 class Item_Inc_Exp_Time;
 class Item_Inc_Life;
-#include "Tile.h"
+// #include "Game.h"
 #include "Character.h"
 #include <iostream>
-#include "SFML/Graphics.hpp"
+
+
 
 
 
@@ -17,12 +20,15 @@ class Item: public Tile
 {
 public:
     Item(int const & row, int const & col);
+    virtual ~Item() = default;
     virtual void give_power_up(Character & character) = 0;    
     virtual void draw_graphics(sf::RenderTexture &) = 0;
     
     int item_row{};
     int item_col{};
     
+    sf::Texture powerup_Texture;
+    sf::Sprite powerup_Sprite;
 };
 
 
@@ -31,7 +37,10 @@ class Item_Inc_Bombs:public Item
 {
 public:
     using Item::Item;
-    ~Item_Inc_Bombs() override = default;
+    ~Item_Inc_Bombs()
+    {
+        std::cout << "Förstör item!" << std::endl;
+    }
     void give_power_up(Character & character) override;
     void draw_graphics(sf::RenderTexture &) override;
 };
@@ -41,7 +50,7 @@ class Item_Inc_Exp_Rad:public Item
 {
 public:
     using Item::Item;
-    ~Item_Inc_Exp_Rad() override = default;
+    ~Item_Inc_Exp_Rad() = default;
     void give_power_up(Character & character) override;	    
     void draw_graphics(sf::RenderTexture &) override; 
 };
@@ -51,7 +60,7 @@ class Item_Inc_Life:public Item
 {
 public:
     using Item::Item;
-    ~Item_Inc_Life() override = default;
+    ~Item_Inc_Life() = default;
     void give_power_up(Character & character) override;    
     void draw_graphics(sf::RenderTexture &) override;
 };
@@ -60,7 +69,7 @@ class Item_Inc_Exp_Time:public Item
 {
 public:
     using Item::Item;
-    ~Item_Inc_Exp_Time() override = default; 
+    ~Item_Inc_Exp_Time() = default; 
     void give_power_up(Character & character) override;
     void draw_graphics(sf::RenderTexture &) override;
 };
